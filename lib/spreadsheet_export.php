34f84a5c19344c628df201537b3a2b93cdfd087d
Alexander Walther <aw@alexplus.de>
2023-06-20 13:36:03 +0200

    initial commit

---
+++ lib/spreadsheet_export.php
@@ -0,0 +1,13 @@
+<?php
+
+class spreadsheet_export
+{
+    // https://github.com/yakamara/redaxo_yform/blob/master/docs/04_yorm.md#yorm-mit-eigener-model-class-verwenden
+    // Nachfolgend ein Beispiel, um eigene Methoden zu erstellen
+    /*
+        public function getName() :string
+        {
+            return $this->getValue('name');
+        }
+    */
+}
--- pages/index.php
+++ pages/index.php
@@ -1,5 +1,5 @@
 <?php
 
-echo rex_view::title(rex_i18n::msg('blaupause_title'));
+echo rex_view::title(rex_i18n::msg('spreadsheet_export_title'));
 
 rex_be_controller::includeCurrentPageSubPath();
