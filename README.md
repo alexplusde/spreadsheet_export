d177b0f0af324fbac04b2280a83270b99a97973c
Alexander Walther <aw@alexplus.de>
2023-06-20 15:56:17 +0200

    Array-Methoden hinzugefügt, README aktualisiert

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # Spreadsheet Export - REDAXO 5 Addon für PHPSpreadsheet
 
+Stellt Tabellen als XLSX-Export zur Verfügung.
+
+```php
+// Sendet export.xlsx direkt als Datei-Download
+spreadsheet_export::sendTable('rex_example', "export");
+
+// Inhalt in eine Datei schreiben, gibt ein rex_file-Objekt zurück
+spreadsheet_export::writeTable('rex_example'); // Gibt ein rex_file-Objekt zurück
+
+// Sendet export.xlsx direkt als Dateidownload anhand einse Arrays, z.B. von rex_sql
+$array = rex_sql::factory()->getArray("SELECT * FROM rex_example");
+spreadsheet_export::sendArray($array, "export");
+
+// Inhalt in eine Datei schreiben, gibt ein rex_file-Objekt zurück
+$array = rex_sql::factory()->getArray("SELECT * FROM rex_example");
+spreadsheet_export::writeArray($array);
+```
+
 ## Lizenz
 
 MIT Lizenz, siehe [LICENSE.md](https://github.com/alexplusde/spreadsheet_export/blob/master/LICENSE.md)  
@@ -15,4 +33,4 @@ https://github.com/alexplusde
 
 ## Credits
 
-<https://github.com/PHPOffice/PhpSpreadsheet>
+<https://github.com/mk-j/PHP_XLSXWriter/>
--- lib/spreadsheet_export.php
+++ lib/spreadsheet_export.php
@@ -2,8 +2,12 @@
 
 class spreadsheet_export
 {
-    public static function sendTable($table) {
+    public static function sendTable($table, $filename = null) {
         
+        if(!rex_sql_table::get($table)->exists()) {
+            exit;
+        }
+
         $data = rex_sql::factory()->getArray('SELECT * FROM '. $table);
         if(count($data)) {
             array_unshift($data, array_keys($data[0]));
@@ -12,15 +16,27 @@ class spreadsheet_export
         $writer = new XLSXWriter();
         $writer->writeSheet($data);
 
-        rex_response::sendResource($writer->writeToString(), "application/vnd.ms-excel", time(),  null, 'attachment', "output.xlsx");
+        rex_response::sendResource($writer->writeToString(), "application/vnd.ms-excel", time(),  null, 'attachment', $filename ?? $table.".xlsx");
         exit;
     }
 
-    public static function writeTable($table, $path = null) {
+    public static function sendArray($array, $filename = null) {
+        
+        $writer = new XLSXWriter();
+        $writer->writeSheet($array);
 
+        rex_response::sendResource($writer->writeToString(), "application/vnd.ms-excel", time(),  null, 'attachment', $filename ?? $table.".xlsx");
+        exit;
+    }
+
+    public static function writeTable($table, $path = null, $filename = null) : ?rex_file {
+
+        if(!rex_sql_table::get($table)->exists()) {
+            return null;
+        }
 
         if(!$path) {
-            rex_path::addonData('spreadsheet_export', $table.'.xlsx');
+            $path = rex_path::addonData('spreadsheet_export', $filename ?? $table.'.xlsx');
         }
         
         $data = rex_sql::factory()->getArray('SELECT * FROM '. $table);
@@ -28,11 +44,25 @@ class spreadsheet_export
             array_unshift($data, array_keys($data[0]));
         }
 
-        rex_file::put($path);
+        rex_file::put($path, "");
           
         $writer = new XLSXWriter();
         $writer->writeSheet($data);
         $writer->writeToFile($path);
-        return;
+        return rex_file::get($path);
+    }
+
+    public static function writeArray($array, $path = null, $filename = null) : ?rex_file {
+
+        if(!$path) {
+            $path = rex_path::addonData('spreadsheet_export', $filename ?? $table.'.xlsx');
+        }
+        
+        rex_file::put($path, "");
+          
+        $writer = new XLSXWriter();
+        $writer->writeSheet($array);
+        $writer->writeToFile($path);
+        return rex_file::get($path);
     }
 }
