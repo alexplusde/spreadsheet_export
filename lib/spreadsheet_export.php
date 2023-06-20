7766fd89d9fc3b07236bb86152f35f54a6d2ec38
Alexander Walther <aw@alexplus.de>
2023-06-20 14:41:16 +0200

    switch vendor

--- lib/spreadsheet_export.php
+++ lib/spreadsheet_export.php
@@ -1,13 +1,30 @@
 <?php
 
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Helper\Downloader;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
 class spreadsheet_export
 {
-    // https://github.com/yakamara/redaxo_yform/blob/master/docs/04_yorm.md#yorm-mit-eigener-model-class-verwenden
-    // Nachfolgend ein Beispiel, um eigene Methoden zu erstellen
-    /*
-        public function getName() :string
-        {
-            return $this->getValue('name');
-        }
-    */
+    public static function sql() {
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+
+
+        $array = rex_sql::factory()->getArray("SELECT id, name FROM rex_article");
+
+
+        $header = array("id", "name");
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->fromArray([$header], $array, 'A1');
+
+        // Dateinamen für die Excel-Datei generieren
+        $filename = 'export.xlsx';
+
+        // Excel-Datei speichern
+        $writer = new Xlsx($spreadsheet);
+        dd($writer);
+    }
 }
